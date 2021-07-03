9cf7034e0e6d9eff6a8c0b5e8e58d873e67348fa
dimaaxxy <dimaaxxy@ya.ru>
2021-07-03 08:40:44 +0700

    #init. Обновление версии

--- src/Pack.php
+++ src/Pack.php
@@ -7,9 +7,12 @@ namespace Sens\Impact;
 class Pack
 {
 
+  const VERSION = '1.0.1';
+
   public static function check()
   {
-    echo "It's work! After update :)";
+    $version = self::VERSION;
+    echo "It's work! After update :)\n Version: $version";
   }
 
 }
